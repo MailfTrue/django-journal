a25fb9b2bae2fc936e13ca0ff211a75fc1de369a
kaushiksk <kaushikskalmady@gmail.com>
2016-12-06 19:15:36 +0530

    changed parseme custom filter to parse till last word

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -5,8 +5,10 @@ register = template.Library()
 
 @register.filter(is_safe=True)
 @stringfilter
-def parseme(value, arg):
-    if len(value)<int(arg):
-        return '%s'%value
-    else:
-        return '%s'%(value[:int(arg)] + '... (Read More)')
+def parseme(value, max_length):
+    if len(value) > int(max_length):
+        new_val = value[:int(max_length)]
+        if not len(value) == int(max_length) + 1 and value[int(max_length)+1]!=" ":
+            new_val = new_val[:new_val.rfind(" ")]
+            return '%s'%(new_val + ' ... (Read More)')
+    return '%s'%value
